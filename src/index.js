fb2644d78ebe75c1c0bdd78e8024607bac711604
Rahulkrpd <rahulkrpd2@gmail.com>
2024-08-02 23:59:58 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import dotenv from "dotenv"
 import express from "express"
+import connectDB from "./db/connetdb.js"
+
+dotenv.config({
+    path:'./env'
+})
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Server is on")
@@ -9,7 +16,7 @@ app.get("/", (req, res) => {
 
 
 // some change are occurs
-app.get("/api/food",(req,res)=>{
+app.get("/api/food", (req, res) => {
     res.send("This is food end point")
 })
 
