a93a995e8897586a182fe609b7279c99d1babc23
Rahulkrpd <rahulkrpd2@gmail.com>
2024-08-23 16:12:52 +0530

    api error handler

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -38,6 +38,7 @@ const userSchema = new mongoose.Schema({
         type: String,  // cloudinary url
 
 
+        
     },
     watchHistory: [{
         type: mongoose.Schema.Types.ObjectId,
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message="Something went wrong",
         errors=[],
-        statck=""
+        stack=""
 
     ){
         super(message)
@@ -14,7 +14,7 @@ class ApiError extends Error{
         this.errors= errors
 
 
-        if(statck)
+        if(stack)
         {
             this.stack= stack
         }
